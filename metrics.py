40de2792094760740abf00d161b3b9e5f9bcf326
sainiansh <saini2004ansh@gmail.com>
2026-01-30 00:41:06 +0530

    Update log_retrieval to include avg_score and latency

--- metrics.py
+++ metrics.py
@@ -1,10 +1,16 @@
 from database import conn
 from datetime import datetime
 
-def log_retrieval(query, chunk_ids, score):
+def log_retrieval(query, chunk_ids, avg_score, latency_ms):
     cursor = conn.cursor()
     cursor.execute(
-        "INSERT INTO retrieval_logs VALUES (?, ?, ?, ?)",
-        (query, ",".join(chunk_ids), score, datetime.now())
+        "INSERT INTO retrieval_logs VALUES (NULL, ?, ?, ?, ?, ?)",
+        (
+            query,
+            ",".join(map(str, chunk_ids)),
+            avg_score,
+            latency_ms,
+            datetime.now()
+        )
     )
     conn.commit()
