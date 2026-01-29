4ee9e4b4855fd4969d9999fc628f094c8a857630
sainiansh <saini2004ansh@gmail.com>
2026-01-30 00:40:30 +0530

    Update reranker.py

--- reranker.py
+++ reranker.py
@@ -1,9 +1,14 @@
+import streamlit as st
 from sentence_transformers import CrossEncoder
 
-reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+@st.cache_resource
+def load_reranker():
+    return CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 
-def rerank(query, chunks):
-    pairs = [(query, c) for c in chunks]
+reranker = load_reranker()
+
+def rerank(query, texts):
+    pairs = [(query, t) for t in texts]
     scores = reranker.predict(pairs)
-    ranked = sorted(zip(chunks, scores), key=lambda x: x[1], reverse=True)
+    ranked = sorted(zip(texts, scores), key=lambda x: x[1], reverse=True)
     return [r[0] for r in ranked[:5]]
